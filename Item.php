28c39f69aaa07935017c8b0362638ede238be617
Guy Marshall <guymarshall9@gmail.com>
2023-04-12 22:03:15 +0100

    Add Item class in Item.php with constructor, getters, and setters

---
+++ Item.php
@@ -0,0 +1,105 @@
+<?php
+
+class Item
+{
+
+    public function __construct(private string $name, private string $description, private int $quantity = 1, private int $price, private DateTime $createdDateTime, private DateTime $editedDateTime)
+    {
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return int
+     */
+    public function getQuantity(): int
+    {
+        return $this->quantity;
+    }
+
+    /**
+     * @param int $quantity
+     */
+    public function setQuantity(int $quantity): void
+    {
+        $this->quantity = $quantity;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPrice(): int
+    {
+        return $this->price;
+    }
+
+    /**
+     * @param int $price
+     */
+    public function setPrice(int $price): void
+    {
+        $this->price = $price;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getCreatedDateTime(): DateTime
+    {
+        return $this->createdDateTime;
+    }
+
+    /**
+     * @param DateTime $createdDateTime
+     */
+    public function setCreatedDateTime(DateTime $createdDateTime): void
+    {
+        $this->createdDateTime = $createdDateTime;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getEditedDateTime(): DateTime
+    {
+        return $this->editedDateTime;
+    }
+
+    /**
+     * @param DateTime $editedDateTime
+     */
+    public function setEditedDateTime(DateTime $editedDateTime): void
+    {
+        $this->editedDateTime = $editedDateTime;
+    }
+}
